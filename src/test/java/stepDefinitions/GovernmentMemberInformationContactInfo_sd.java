d3b7a1cb8866a963d9459340fb122fcdb0daddbc
AG53052 <AG53052@LBF6T1X2.us.ad.wellpoint.com>
2021-05-17 12:46:19 +0530

    Removed unused imports

--- src/test/java/stepDefinitions/GovernmentMemberInformationContactInfo_sd.java
+++ src/test/java/stepDefinitions/GovernmentMemberInformationContactInfo_sd.java
@@ -1,7 +1,6 @@
 package stepDefinitions;
 
 import io.cucumber.java.en.Then;
-import pageObjects.GovernmentMemberInformationContactInfo;
 
 public class GovernmentMemberInformationContactInfo_sd extends BaseClass {
 
