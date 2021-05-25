27f9785f98ded497a189224fe367173d5388ceb1
Geethanandan, Aarisetty <Aarisetty.Geethanandan@legatohealth.com>
2021-05-25 12:50:04 -0400

    Runner.java edited online with Bitbucket

--- src/test/java/testRunner/Runner.java
+++ src/test/java/testRunner/Runner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src\\test\\resources\\features",
+@CucumberOptions(features = "src/test/resources/features",
 								glue = {"stepDefinitions","driver"},
 								dryRun = false,
 								plugin = {"pretty", "html:report/CPSUI_Report.html", 
